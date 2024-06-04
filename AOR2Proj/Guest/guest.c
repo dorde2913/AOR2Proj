d1800798098e47f7c881ce9c37e327549e5dd805
dorde <dj13372913@gmail.com>
2024-06-04 20:53:28 +0200

    dodata podrska za IO

--- AOR2Proj/Guest/guest.c
+++ AOR2Proj/Guest/guest.c
@@ -1,4 +1,9 @@
+#include <stddef.h>
+#include <stdint.h>
 
+static void outb(uint16_t port, uint8_t value) {
+    asm("outb %0,%1" : /* empty */ : "a" (value), "Nd" (port) : "memory");
+}
 void
 __attribute__((noreturn))
 __attribute__((section(".start")))
@@ -7,6 +12,16 @@ _start(void) {
 	/*
 		INSERT CODE BELOW THIS LINE
 	*/
+
+    const char *p;
+    uint16_t port = 0xE9;
+    uint8_t value = 'E';
+
+    asm("outb %0,%1" : /* empty */ : "a" (value), "Nd" (port) : "memory");
+    for (p = "Hello, world!\n"; *p; ++p)
+        outb(0xE9, *p);
+
+
 
 	/*
 		INSERT CODE ABOVE THIS LINE
